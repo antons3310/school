27b460be4adca2762ca2399aede6c7ce0aa903ca
antoshka <ashebalkin@diasoft.ru>
2022-02-27 23:50:50 +0300

    Решение ДЗ

--- src/main/java/ru/hogwarts/ashebalkin/school/controller/AvatarController.java
+++ src/main/java/ru/hogwarts/ashebalkin/school/controller/AvatarController.java
@@ -17,7 +17,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 @RestController
-@RequestMapping("student")
+@RequestMapping("avatar")
 public class AvatarController {
 
     private final AvatarService avatarService;
